5e86200bf782ec37cd638c9ce4ef8be17cbe23f1
Teddy Waweru <teddywaweru@gmail.com>
2025-04-05 20:18:36 -0500

    playerctl details for polybar

--- .scripts/polybar_playerctl.sh
+++ .scripts/polybar_playerctl.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
-set -e
+# title=$(playerctl metadata | grep "title" | awk -F "  " '{print $(NF-0)}')
+TITLE=$(playerctl metadata title -s | head -c 36)
+ARTIST=$(playerctl metadata artist -s)
 
-title=$( playerctl metadata |grep "title" | awk -F "  " '{print $(NF-0)}' )
-
-
-echo "${title}"
+if [[ $? -eq 0 ]]; then
+	PLAYER_DETAILS=$m echo "$ARTIST - $TITLE"
+else
+	PLAYER_DETAILS=$m echo ""
+fi
