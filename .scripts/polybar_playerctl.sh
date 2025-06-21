c177f78c2117a281130271b05352646e78dabd08
Teddy Waweru <teddywaweru@gmail.com>
2025-06-20 22:20:35 -0500

    configure playerctl for polybar

--- .scripts/polybar_playerctl.sh
+++ .scripts/polybar_playerctl.sh
@@ -1,8 +1,17 @@
 #!/bin/zsh
 
 # title=$(playerctl metadata | grep "title" | awk -F "  " '{print $(NF-0)}')
-TITLE=$(playerctl metadata title -s | head -c 36)
-ARTIST=$(playerctl metadata artist -s)
+players=$(playerctl metadata -l)
+while IFS= read -r line; do
+	if [ "$line" = "spotify_player" ]; then
+	player="spotify_player"
+else
+	player=""
+	fi
+done <<<"$players"
+
+TITLE=$(playerctl metadata --player="$player" title -s | head -c 36)
+ARTIST=$(playerctl metadata --player="$player" artist -s)
 
 if [[ $? -eq 0 ]]; then
 	PLAYER_DETAILS=$m echo "$ARTIST - $TITLE"
